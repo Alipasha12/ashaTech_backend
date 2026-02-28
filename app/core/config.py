86555f117767872b1008f55615860a2cfc8bd3fe
ali abbas <alipasha7017@gmail.com>
2026-02-28 12:17:41 +0530

    adding requirements

--- app/core/config.py
+++ app/core/config.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     SECRET_KEY : str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 15
+    PORT: int = 8000
     
     model_config = SettingsConfigDict(env_file=".env")
     
